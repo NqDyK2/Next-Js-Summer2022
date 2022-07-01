575f74f929c9e146ba447ce90352ff0dad5dd3fa
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-07-01 09:28:32 +0700

    feat: update callApi in blogs page and detail blog

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,14 +1,47 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-type Props = {}
+type BlogProps = {
+  blog: any
+}
 
-const BlogDetail = (props: Props) => {
-    const router = useRouter()
-    console.log(router.query);
-    return (
-    <div className='text-red-700'>BlogDetail </div>
+const BlogDetail = ({blog}: BlogProps) => {
+  if(!blog) return null;
+
+  return (
+    <div className='text-red-700'>BlogDetail - {blog.name} </div>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await (await fetch(`http://localhost:3001/blogs`)).json();
+  const paths = data.map(item => {
+    return { params: { id: item.id } }
+  })
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+export const getStaticProps: GetStaticProps<BlogProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log("Hello Server!!!");
+  const data = await (await fetch(`http://localhost:3001/blogs/${context.params.id}`)).json();
+  console.log("Data:", data);
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      blog: data
+    }
+  }
 }
 
 export default BlogDetail
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,43 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
-type Props = {}
+type BlogProps = {
+  blogs: any[]
+}
 
-const BlogPage = (props: Props) => {
+const BlogPage = ({ blogs }: BlogProps) => {
+  console.log("This is client");
+  
+  console.log("Blogs: ", blogs);
+  
   return (
-    <div>BlogPage</div>
+    <div>
+      {blogs.map((item) => (
+        <div key={item.id}><Link href={`/blog/${item.id}`}>{item.name}</Link></div>
+      ))
+
+      }
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<BlogProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log("Hello Server!!!");
+  const data = await(await fetch(`http://localhost:3001/blogs`)).json();
+  console.log("Data:" , data);
+  if(!data){
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      blogs: data
+    }
+  }
 }
 
 export default BlogPage
